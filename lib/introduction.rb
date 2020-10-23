9c7d1c53175f0948cfd046134505add556feaad6
Adri Torres <adriana.amaral@msn.com>
2020-10-23 17:50:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,21 +3,21 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction ("Josh")
+
 
 def introduction_with_language (name, language)
   
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-introduction_with_language ("Josh","Ruby")
+
 
 def introduction_with_language_optional (name, language = Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
 
-introduction_with_language_optional
+
   
   
 
